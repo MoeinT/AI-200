fb01d5651cac82069f785f651b40ceeb04906739
MoeinT <moin.torabi@gmail.com>
2024-06-07 11:04:40 +0200

    fixed config

--- azure-ai-vision/image-analysis/tf/azure/AIServices.tf
+++ azure-ai-vision/image-analysis/tf/azure/AIServices.tf
@@ -1,7 +1,7 @@
 module "AzureAIServices" {
   source = "../../../../terraform-modules/azure-ai-services"
   properties = {
-    "azure-ai-services-ai-200-1-${var.env}" = {
+    "azure-ai-services-ai-200-${var.env}" = {
       location            = module.Rg.rg-locations["ai-102-${var.env}"],
       resource_group_name = module.Rg.rg-names["ai-102-${var.env}"],
       kind                = "CognitiveServices"
